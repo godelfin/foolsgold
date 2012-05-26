16ce1a0804d8364740c7c66c7e4f7688a3b52457
ide user ide_godelfin <tovar.brian@gmail.com>
2012-05-26 21:57:52 +0000

    just another hello world

---
+++ example.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var circle = require('circle');
+var node_oauth = require('node-oauth');
+console.log(node_oauth);
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('The area of a circle of radius 4 is ' + circle.area(4));
+  //response.end('Hello World\n');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
+
